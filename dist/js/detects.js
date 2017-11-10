cbd8dbcc0d19248d32ab7f73fb8ee173b88c53c1
Roel Van Gils <roelvangils@gmail.com>
2017-11-10 19:13:28 +0100

    Added some curly braces to prevent Gulp throwing a warning

--- dist/js/detects.js
+++ dist/js/detects.js
@@ -11,7 +11,7 @@
 
 	// SVG feature detection
 	var supports = !!document.createElementNS && !!document.createElementNS('http://www.w3.org/2000/svg', 'svg').createSVGRect;
-	if ( !supports ) return;
+	if ( !supports ) { return; }
 
 	// Add `.svg` class to <html> element
 	document.documentElement.className += (document.documentElement.className ? ' ' : '') + 'svg';
